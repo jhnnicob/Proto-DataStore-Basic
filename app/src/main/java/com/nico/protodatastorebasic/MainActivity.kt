8ac2d00565cd6453a478f45524bad586f4849dc7
Nico <johnnicomolon@yahoo.com>
2022-01-10 10:28:03 +0800

    create function createWithFactory

--- app/src/main/java/com/nico/protodatastorebasic/MainActivity.kt
+++ app/src/main/java/com/nico/protodatastorebasic/MainActivity.kt
@@ -5,21 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.nico.protodatastorebasic.datastore.SettingsProtoDataStore
 import com.nico.protodatastorebasic.datastore.UserDataProtoDataStore
-import com.nico.protodatastorebasic.model.User
-import com.nico.protodatastorebasic.ui.theme.ProtoDataStoreBasicTheme
-import kotlinx.coroutines.launch
+import com.nico.protodatastorebasic.presentation.example.ExampleScreen
+import com.nico.protodatastorebasic.presentation.ui.theme.ProtoDataStoreBasicTheme
+import com.nico.protodatastorebasic.presentation.user_data.UserDataScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,125 +27,31 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Column(modifier = Modifier.fillMaxWidth()) {
-                        SettingScreen(protoDataStore = settingsProto)
-                        UserDataScreen(protoDataStore = userDataProto)
+                        ExampleScreen(protoDataStore = settingsProto)
+                        UserDataScreen(viewModel = mainViewModel, protoDataStore = userDataProto)
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun SettingScreen(
-    protoDataStore: SettingsProtoDataStore
-) {
-
-    val counter = protoDataStore.counterFlow.asLiveData().observeAsState().value.toString()
-
-    val scope = rememberCoroutineScope()
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(onClick = {
-            scope.launch {
-                protoDataStore.incrementCounter()
+    private fun createWithFactory(
+        create: () -> ViewModel
+    ): ViewModelProvider.Factory {
+        return object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return create.invoke() as T
             }
-        }) {
-            Text(text = "Increment")
         }
-        Spacer(modifier = Modifier.padding(vertical = 10.dp))
-
-        Text(text = counter)
-
-        Spacer(modifier = Modifier.padding(vertical = 20.dp))
-    }
-}
-
-@Composable
-fun UserDataScreen(
-    protoDataStore: UserDataProtoDataStore
-) {
-
-    val userData = protoDataStore.userDataList.asLiveData().observeAsState().value
-
-    val username = remember {
-        mutableStateOf("")
     }
 
-    val userType = remember {
-        mutableStateOf("")
-    }
-
-    val token = remember {
-        mutableStateOf("")
-    }
-
-    val scope = rememberCoroutineScope()
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp)
-    ) {
-        TextField(
-            value = username.value,
-            onValueChange = {
-                username.value = it
-            },
-            modifier = Modifier.fillMaxWidth(),
-            label = { 
-                Text(text = "Enter username")
+    private val mainViewModel: MainViewModel by lazy {
+        ViewModelProvider(
+            this,
+            createWithFactory {
+                MainViewModel(protoDataStore = UserDataProtoDataStore(applicationContext))
             }
-        )
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        TextField(
-            value = userType.value,
-            onValueChange = {
-                userType.value = it
-            },
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = "Enter usertype")
-            }
-        )
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        TextField(
-            value = token.value,
-            onValueChange = {
-                token.value = it
-            },
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = "Enter user token")
-            }
-        )
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        Button(onClick = {
-            scope.launch {
-                protoDataStore.saveUserDataList(User(username.value, userType.value, token.value))
-            }
-        }) {
-            Text(text = "Save")
-        }
-
-        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-
-        userData?.map { user ->
-            Text(text = "Username: ${user.username}")
-            Text(text = "Usertype: ${user.userType}")
-            Text(text = "Token: ${user.token}")
-        }
-
+        )[MainViewModel::class.java]
     }
+
 }
