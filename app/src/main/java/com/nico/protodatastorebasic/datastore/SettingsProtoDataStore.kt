8c07c786482633fd17b44ffe3284318d9a41a8e8
Nico <johnnicomolon@yahoo.com>
2022-01-10 09:11:44 +0800

    add runBlocking

--- app/src/main/java/com/nico/protodatastorebasic/datastore/SettingsProtoDataStore.kt
+++ app/src/main/java/com/nico/protodatastorebasic/datastore/SettingsProtoDataStore.kt
@@ -3,9 +3,7 @@ package com.nico.protodatastorebasic.datastore
 import android.content.Context
 import com.nico.protodatastorebasic.datastore.SettingsSerializer.settingsDataStore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 
 class SettingsProtoDataStore(
     private val context: Context
@@ -15,8 +13,6 @@ class SettingsProtoDataStore(
         .map { setting ->
             setting.exampleCounter
         }
-
-    val counter = runBlocking { context.settingsDataStore.data.first() }
 
     suspend fun incrementCounter() {
         context.settingsDataStore.updateData { currentSettings ->
